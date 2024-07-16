f516d0e0770eb1f592cb333e6f766d5dfd613993
Lomakin Ivan <74ivan7474@gmail.com>
2024-07-16 17:56:58 +0500

    Update urls.py

--- Django/first-project/first_project/first_project/urls.py
+++ Django/first-project/first_project/first_project/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from app.views import home_view, time_view, workdir_view
 
 
@@ -24,7 +24,7 @@ urlpatterns = [
     # обрабатывались Django
     # path('current_time/', time_view, name='time'),
     # path('workdir/', workdir_view, name='workdir'),
-    path('admin'/, admin.site.urls),
+    path('admin/', admin.site.urls),
     path('time/', time_view, name='time'),
-    path('workdir/', workdir_view, name='workdir')
+    path('workdir/', workdir_view, name='workdir'),
 ]
